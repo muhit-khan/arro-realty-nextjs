36a317399b317e50d84de0f84dec798f248f533e
Muhit Khan <muhitkhan_bd@outlook.com>
2024-06-20 09:26:47 +0600

    Updated Hyperlink of Footer and Navbar

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../../public/images/logo.png';
 import { RiFacebookFill, RiInstagramFill, RiYoutubeFill, RiTwitterFill } from 'react-icons/ri';
 
@@ -18,20 +19,20 @@ const Footer = () => {
                 <div className='footer-col-2 flex flex-col items-start p-4'>
                     <h3 className='text-lg text-amber-500 font-semibold mb-2'>Quick Links</h3>
                     <ul>
-                        <li className='  hover:text-amber-500 active:text-amber-600 mb-2'>Home</li>
-                        <li className='  hover:text-amber-500 active:text-amber-600 mb-2'>About</li>
-                        <li className='  hover:text-amber-500 active:text-amber-600 mb-2'>Service</li>
-                        <li className='  hover:text-amber-500 active:text-amber-600 mb-2'>Contact</li>
-                        <li className='  hover:text-amber-500 active:text-amber-600 mb-2'>Find Properties</li>
+                        <li className='hover:text-amber-500 active:text-amber-600 mb-2'><Link href='/'>Home</Link></li>
+                        <li className='hover:text-amber-500 active:text-amber-600 mb-2'><Link href='/about'>About</Link></li>
+                        <li className='hover:text-amber-500 active:text-amber-600 mb-2'><Link href='/service'>Service</Link></li>
+                        <li className='hover:text-amber-500 active:text-amber-600 mb-2'><Link href='/contact'>Contact</Link></li>
+                        <li className='hover:text-amber-500 active:text-amber-600 mb-2'><Link href='/properties'>Find Properties</Link></li>
                     </ul>
                 </div>
                 <div className='footer-col-3 flex flex-col items-start p-4'>
                     <h3 className='text-lg text-amber-500 font-semibold mb-2'>Follow Me</h3>
                     <ul>
-                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiFacebookFill /> <span className='ml-2'>Facebook</span></li>
-                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiInstagramFill /> <span className='ml-2'>Instagram</span></li>
-                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiYoutubeFill /> <span className='ml-2'>Youtube</span></li>
-                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiTwitterFill /> <span className='ml-2'>Twitter</span></li>
+                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiFacebookFill /> <Link href='https://linkedin.com/in/muhit-khan' className='ml-2' target="_blank">Facebook</Link></li>
+                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiInstagramFill /> <Link href='https://linkedin.com/in/muhit-khan' className='ml-2' target="_blank">Instagram</Link></li>
+                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiYoutubeFill /> <Link href='https://linkedin.com/in/muhit-khan' className='ml-2' target="_blank">Youtube</Link></li>
+                        <li className='flex hover:text-amber-500 active:text-amber-600 items-center mb-2'><RiTwitterFill /> <Link href='https://linkedin.com/in/muhit-khan' className='ml-2' target="_blank">Twitter</Link></li>
                     </ul>
                 </div>
             </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../../public/images/logo.png';
 
 const Navbar = () => {
@@ -24,17 +25,21 @@ const Navbar = () => {
     return (
         <nav className={`fixed top-0 left-0 w-full bg-black shadow-md z-50 transition-opacity duration-300 ${scrolled ? 'bg-opacity-90' : 'bg-opacity-50'}`}>
             <div className="container mx-auto flex items-center justify-between py-1 px-10">
-                <Image src={logo} alt="AH" className="w-44" />
+                <Link href='/'>
+                    <Image src={logo} alt="Logo" className="w-44" />
+                </Link>
                 <ul className="flex items-center space-x-6">
-                    <li className="hover:text-amber-600 active:text-amber-500">Home</li>
-                    <li className="hover:text-amber-600 active:text-amber-500">About</li>
-                    <li className="hover:text-amber-600 active:text-amber-500">Team</li>
-                    <li className="hover:text-amber-600 active:text-amber-500">Services</li>
-                    <li className="hover:text-amber-600 active:text-amber-500">Contact</li>
+                    <li className="hover:text-amber-600 active:text-amber-500"><Link href='/'>Home</Link></li>
+                    <li className="hover:text-amber-600 active:text-amber-500"><Link href='/about'>About</Link></li>
+                    <li className="hover:text-amber-600 active:text-amber-500"><Link href='/services'>Services</Link></li>
+                    <li className="hover:text-amber-600 active:text-amber-500"><Link href='/team'>Team</Link></li>
+                    <li className="hover:text-amber-600 active:text-amber-500"><Link href="/contact">Contact</Link></li>
                     <li>
-                        <button className="bg-amber-500 font-semibold text-gray-900 py-2 px-6 rounded-full hover:bg-amber-600 active:bg-amber-500">
-                            Find Properties
-                        </button>
+                        <Link href="/properties">
+                            <button className="bg-amber-500 font-semibold text-gray-900 py-2 px-6 rounded-full hover:bg-amber-600 active:bg-amber-500">
+                                Find Properties
+                            </button>
+                        </Link>
                     </li>
                 </ul>
             </div>
